25d4a70de668eb8fca36b65b8617a38d1ba242ce
Alan Krajina <alankrajina88@gmail.com>
2019-07-01 15:03:40 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
     show = Show.maximum("rating")
   end
   def self.most_popular_show
-    show = Show.find_by(rating: self.highest_rating)
+    show = Show.find_by(rating: highest_rating)
   end
 end
